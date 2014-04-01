325b89f5d2057b345e73b23e8a15934fabd1fc84
aldwynology <aldwyn.up@gmail.com>
2014-04-01 16:26:39 +0800

    added draggable text-and-circle to svg

--- assets/js/visualizer.js
+++ assets/js/visualizer.js
@@ -1,47 +1,83 @@
 $(document).ready( function(){
 
-	$stage = $('svg');
-	$off_top = $stage.offset().top;
-	$off_left = $stage.offset().left;
-	$x = 0;
-	$y = 0;
-	$count = 0;
-	$flag = false;
-	$selected = null;
+	SVG = $('svg');
+	off_top = SVG.offset().top;
+	off_left = SVG.offset().left;
+	x = 0;
+	y = 0;
+	count = 0;
+	selected = null;
+	openState = null;
 
-	$stage.on('contextmenu', 'circle', function(e){
+	$(document).on('contextmenu', 'circle', function(e){
+		e.preventDefault();
 	   alert('Context Menu event has fired!');
 	   return false;
 	});
 
-	$stage.on('mousedown', 'circle', function(e){
-		$flag = true;
-		$selected = $(this);
+	SVG.on('mousedown', 'g', function(e){
+		selected = $(this);
+		console.log(e.which);
 	});
 
-	$stage.on('mousemove', $stage, function(e){
-		if ($selected != null)
-			$selected.attr({'cx': e.pageX - $off_left, 'cy': e.pageY - $off_top});
-	});
+	SVG.on('mouseup', 'g',  function(e){
+		if (selected != null) {
+			if (openState != null) {
 
-	$(document).mouseup(function(e){
-		$flag = false;
-		if ($selected != null)
-			$selected.attr({'cx': e.pageX - $off_left, 'cy': e.pageY - $off_top});
-		$selected = null;
-	});
-
-	$stage.on('mousedown', function(e){
-		if ($selected == null) {
-			$x = e.pageX - $off_left;
-			$y = e.pageY - $off_top;
-
-			$stage.append(
-				$(document.createElementNS('http://www.w3.org/2000/svg', 'circle')).attr(
-					{'id': 'q'+$count, 'cx': $x, 'cy': $y, 'r': 30, 'stroke': 'black', 'stroke-width': 1, 'fill': 'white'})
-			);
-			$count++;	
+			}
+			openState = $(this);
+			$('line').attr({'x1': e.pageX - off_left, 'y1': e.pageY - off_top});
 		}
 	});
 
+	SVG.on('mousemove', SVG, function(e){
+		if (selected != null) {
+			x = e.pageX - off_left;
+			y = e.pageY - off_top;
+			selected.find('text').attr({
+				'x': x - selected.find('text').width()/2,
+				'y': y - selected.find('text').height()*2
+			});
+			selected.find('circle').attr({'cx': x, 'cy': y});
+		}
+	});
+
+	SVG.on('mouseup', function(e){
+		if (selected != null) {
+			x = e.pageX - off_left;
+			y = e.pageY - off_top;
+			selected.find('text').attr({
+				'x': x - selected.find('text').width()/2,
+				'y': y - selected.find('text').height() + 50
+			});
+			selected.find('circle').attr({'cx': x, 'cy': y});
+		}
+		selected = null;
+	});
+
+	SVG.on('mousedown', function(e){
+		if (selected == null) {
+			x = e.pageX - off_left;
+			y = e.pageY - off_top;
+
+			SVG.append($(document.createElementNS('http://www.w3.org/2000/svg', 'g')).attr({'id': 'q'+count, 'x': x, 'y': y}));
+
+			$('#q'+count).append(
+				$(document.createElementNS('http://www.w3.org/2000/svg', 'circle')).attr(
+					{'cx': x, 'cy': y, 'r': 30, 'stroke': 'black', 'stroke-width': 1, 'fill': 'white'})
+			).append(
+				$(document.createElementNS('http://www.w3.org/2000/svg', 'text')).attr(
+					{'x': x, 'y': y, 'stroke': 'black', 'fill': 'black'}).html('Q'+count)
+			);
+
+			textWidth = $('#q'+count+' text').width()/2;
+			textHeight = $('#q'+count+' text').height()/4;
+			$('#q'+count+' text').attr({'x': x - textWidth, 'y': y + textHeight});
+			count++;	
+		}
+	});
+
+	SVG.on('mousemove', SVG, function(e){
+		$('line').attr({'x2': e.pageX - off_left, 'y2': e.pageY - off_top});
+	});
 });
